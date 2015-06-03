e756eb71f045c9f3e66516273eabd7101fc115e4
esmith <eoinsmith001@gmail.com>
2015-06-04 00:41:20 +0100

    syntax in server js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app     = express();
 
 
-// dev test prod
-var env = process.env.NODE_ENV || 'development';
+// dev test prod ?
+var env    = process.env.NODE_ENV || 'development';
 var config = require('./config/config')[env];
-var port = process.env.PORT || config.port || 8080;
+var port   = process.env.PORT || config.port || 8080;
 
 var bodyParser = require('body-parser');
 
@@ -21,6 +21,7 @@ mongoose.createConnection(config.db_uri, config.db_options, function(err) {
 });
 
 var morgan = require('morgan');
+var User   = require('./app/models/user');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -39,6 +40,13 @@ app.get('/',function(req,res) {
 });
 
 var apiRouter = express.Router();
+
+// universal middleware for api
+apiRouter.use(function(req,res,next) {
+  console.log('api request incoming');
+  next();
+});
+
 apiRouter.get('/',function(req,res) {
   res.json({message: 'welcome to the api'});
 });
